650dc4060e6d80ac047028de5fbe8212b625ea5f
vsangleis <vsangleis@users.noreply.github.com>
2025-11-11 13:02:19 +0000

    Add core-constants.less, email-src-queries.rs, interface-provider.js

---
+++ fragments-export/validator-util/email-src-queries.rs
@@ -0,0 +1,3 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/track_fhe/fragments-export/validator-util/email-src-queries.rs
+// Generated at: 2025-12-03T23:45:48.332Z
+
---
+++ fragments-export/validator-util/interface-provider.js
@@ -0,0 +1,4 @@
+export const config = { runtime: 'edge' };
+
+export default function statistics() { return <div>Component</div>; }
+
